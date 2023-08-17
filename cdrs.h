9bf1e47e28a3014823b6330d6acb14c3135ae800
Matheus De Jesus Pereira <92840509+TeuZero@users.noreply.github.com>
2023-08-16 21:09:52 -0300

    cdrs.h

---
+++ cdrs.h
@@ -0,0 +1,37 @@
+/*
+    CabeÃ§alho dos recursos
+*/
+
+// ICONE
+#define IDI_MEUICONE 20
+
+// BOTÃ•ES
+#define IDC_TEXTBOX 50
+#define IDC_LOADLL 100
+#define IDC_STATIC 51
+#define IDC_INJECT 1024
+#define BUFFER_SIZE 128
+#define IDC_TEUZERO 204
+
+// ListBox
+#define IDC_LISTBOX 102
+
+// Handle Main
+HWND hMainWindow;
+HINSTANCE hMainInstance;
+
+// listBox
+HWND hListBox;
+
+// Caixa de texto entrada
+HWND hTextInput;
+
+// Caixa de texto de saida
+HWND hLblOutput;
+
+// BotÃµes
+HWND hLoadDll;
+HWND hInject;
+
+//TEXTO
+HWND hTeuzero;
